2af96902ea300a2da3dcd6dc9d3648f5c1ae7ee2
Sebastian Ullrich <sebasti@nullri.ch>
2013-04-09 20:15:25 +0200

    Allow configuration of the number of messages to backtrack

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Applies given s/// commands to the previous message.
+Applies given s/// commands to the previous message. If the previous message doesn't match the search pattern, SedBot will continue with the message before it. Use `!config SedBot` to set the maximum `BACKLOG_LENGTH`, the default is 3.
 
 Usage:
 
--- sedBot.py
+++ sedBot.py
@@ -8,19 +8,35 @@ from errbot.utils import get_sender_username
 class SedBot(BotPlugin):
 	def __init__(self):
 		super(SedBot, self).__init__()
-		self.last_mess = None
+		self.backlog = []
+
+	def configure(self, configuration):
+		super().configure(configuration)
+		self.config = self.config or self.get_configuration_template()
+
+	def get_configuration_template(self):
+		return {'BACKLOG_LENGTH': 3}
+
+	def check_configuration(self, configuration):
+		super().check_configuration(configuration)
+
+		if configuration['BACKLOG_LENGTH'] <= 0:
+			raise ValidationException('BACKLOG_LENGTH must be positive')
 
 	def callback_message(self, conn, mess):
-		if self.last_mess:
-			command = re.match('s/([^/]+)/([^/]*)/?', mess.getBody())
-			if command:
+		command = re.match('s/([^/]+)/([^/]*)/?', mess.getBody())
+		if command:
+			for old_mess in self.backlog:
 				try:
-					(replaced, n) = re.subn(command.group(1), command.group(2), self.last_mess.getBody())
+					(replaced, n) = re.subn(command.group(1), command.group(2), old_mess.getBody())
 				except Exception as e:
 					self.send(mess.getFrom(), "regex does not compute", message_type=mess.getType())
 					return
 
 				if n > 0:
-					reply = '{0} meant: "{1}"'.format(get_sender_username(self.last_mess), replaced)
+					reply = '{0} meant: "{1}"'.format(get_sender_username(old_mess), replaced)
 					self.send(mess.getFrom(), reply, message_type=mess.getType())
-		self.last_mess = mess
+					return
+
+		self.backlog.insert(0, mess)
+		self.backlog = self.backlog[:self.config['BACKLOG_LENGTH']]
